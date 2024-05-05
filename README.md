6b3707acb435d00138125c98482d28592b268021
Camille PD <52207575+cpreeldumas@users.noreply.github.com>
2024-05-05 17:15:56 -0400

    copy over class 5 project

---
+++ README.md
@@ -0,0 +1,14 @@
+# LL97 Housing Equity Map
+
+This repository contains the HTML, JavaScript, and CSS code behind the [LL97 Housing Equity Map](https://cpreeldumas.github.io/class-5-project/), an interactive web map of NYC census tracts with [Local Law 97](https://www.nyc.gov/site/sustainablebuildings/ll97/local-law-97.page)-covered multifamily housing buildings categorized into four quadrants by their level of rent burden and greenhouse gas emissions.
+
+The map relies on data analysis of the EPA's latest [Energy and Water Data Disclosure for Local Law 84](https://data.cityofnewyork.us/Environment/Energy-and-Water-Data-Disclosure-for-Local-Law-84-/7x5e-2fxh/about_data) and the 2022 5-Year American Community Survey. More details on the data and methods can will be hosted in the "Methods" page of the map, which is still under construction. 
+
+# Map Home View
+
+![Screenshot 2024-05-01 at 10 41 40 PM](https://github.com/cpreeldumas/class-5-project/assets/52207575/b0832b21-52aa-4adc-9288-21e9ad8a5e51)
+
+# Map Interactivity
+The map lets users zoom, pan, and explore the census tracts. When a user clicks on a tract, the tract is highlighted and a table with additional data is dispayed in the left-side pane. A future iteration of this map will display additional data in this table, with a more in-depth exploration of at-risk tracts.
+
+![Screenshot 2024-05-01 at 10 49 55 PM](https://github.com/cpreeldumas/class-5-project/assets/52207575/fb4937c5-0080-4d37-9161-2ca86cd13b45)
---
+++ js/scripts.js
@@ -0,0 +1,215 @@
+
+function openAboutPopup() {
+    document.getElementById("nav-about-popup").style.display = "block";
+    document.getElementById("nav-popup-overlay").style.display = "block";
+}
+
+function openMethodsPopup() {
+    document.getElementById("nav-methods-popup").style.display = "block";
+    document.getElementById("nav-popup-overlay").style.display = "block";
+}
+
+function closeAboutPopup() {
+    document.getElementById("nav-about-popup").style.display = "none";
+    document.getElementById("nav-popup-overlay").style.display = "none";
+}
+
+function closeMethodsPopup() {
+    document.getElementById("nav-methods-popup").style.display = "none";
+    document.getElementById("nav-popup-overlay").style.display = "none";
+}
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3ByZWVsZHVtYXMiLCJhIjoiY2x1bHZ0dGJ0MGw4bTJpbGxwdm5jZmQ4cyJ9.ZusuHjH6O73kH4F2RmenWA';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v11', // dark basemap
+    center: [-73.96143, 40.73941], // starting position [lng, lat]
+    zoom: 10, // starting zoom
+});
+
+
+map.addControl(new mapboxgl.NavigationControl());
+
+// when the map is finished it's initial load, add sources and layers.
+map.on('load', function () {
+
+
+    // add a geojson source for choropleth
+    map.addSource('map-data-tract', {
+        type: 'geojson',
+        data: 'data/map_data_tract.geojson',
+    })
+
+    // Create LabelLayerID for 3D Buildings
+    const layers = map.getStyle().layers;
+    const labelLayerId = layers.find(
+        (layer) => layer.type === 'symbol' && layer.layout['text-field']
+    ).id;
+
+    // first add the fill layer, using a match expression to give each a unique color based on its quadrant property
+    map.addLayer({
+        id: 'map-data-tract-fill',
+        type: 'fill',
+        source: 'map-data-tract',
+        paint: {
+            'fill-color': [
+                'match',
+                ['get', 'qd_2024'],
+                'stable_highrent', '#757FBD',
+                'stable_lowrent', '#BDC4E3',
+                'disadvantaged', '#F89638',
+                'equipped', '#F9E2B4',
+                '#ccc'
+            ],
+            'fill-opacity': 0.7
+
+        }
+    },
+    labelLayerId
+)
+
+    // Add a layer for highlighting the clicked polygon
+    map.addLayer({
+        id: 'highlighted-tract',
+        type: 'line',
+        source: 'map-data-tract',
+        paint: {
+            'line-color': 'white',
+            'line-width': 2,
+            'line-opacity': 0.8,
+            'line-blur': 0.3
+        },
+        filter: ['==', 'GEOID', ''] // Initially filter to none
+    });
+
+    // When a click event occurs on a feature in the states layer,
+    // open a popup at the location of the click, with description
+    map.on('click', 'map-data-tract-fill', (e) => {
+
+        // Highlight the clicked polygon
+        map.setFilter('highlighted-tract', ['==', 'GEOID', e.features[0].properties.GEOID]);
+
+        // Open a popup with information about the clicked polygon
+        /*new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(`
+            <h3>${e.features[0].properties.GEOID}</h3>
+            <p>
+                <strong>Median GHG Intensity:</strong> ${e.features[0].properties.mdn_ghg} kgCO2e/ft2<br>
+                <strong>Rent Burden:</strong> Median rent is ${e.features[0].properties.rntbrdE} % of household income <br>
+            </p>
+        `)
+            .addTo(map);
+        */
+
+        // Populate the table with data specific to the clicked polygon
+        const tableData = {
+            "Census Tract": e.features[0].properties.GEOID,
+            "Median GHG Intensity": `${e.features[0].properties.mdn_ghg} kgCO2e/ft2`,
+            "Rent Burden": `Median rent is ${e.features[0].properties.rntbrdE}% of household income`
+        };
+
+        const table = document.getElementById('data-table');
+
+        table.innerHTML = '';
+
+        for (const [key, value] of Object.entries(tableData)) {
+            const row = table.insertRow();
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            cell1.textContent = key;
+            cell2.textContent = value;
+
+            // Add borders to table cells
+            cell1.style.border = '1px solid #ccc';
+            cell2.style.border = '1px solid #ccc';
+            cell1.style.padding = '8px';
+            cell2.style.padding = '8px';
+        }
+
+        document.getElementById('sidebar').style.display = 'block';
+    });
+
+    // Change the cursor to a pointer when
+    // the mouse is over the states layer.
+    map.on('mouseenter', 'map-data-tract-fill', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change the cursor back to a pointer
+    // when it leaves the states layer.
+    map.on('mouseleave', 'map-data-tract-fill', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+
+    // The 'building' layer in the Mapbox Streets
+    // vector tileset contains building height data
+    // from OpenStreetMap.
+    map.addLayer(
+        {
+            'id': 'add-3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+                // Use an 'interpolate' expression to
+                // add a smooth transition effect to
+                // the buildings as the user zooms in.
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        }
+    );
+
+
+
+
+
+
+})
+
+// Fly to Random "At Risk" Tract
+/*function flyToRandomRisk() {
+    // Filter features based on the "risk" category
+    const riskFeatures = map.querySourceFeatures('map-data-tract', {
+        filter: ['==', ['get', 'qd_2024'], 'disadvantaged']
+    });
+
+    // Check if there are any features in the "risk" category
+    if (riskFeatures.length > 0) {
+        // Randomly select one feature from the filtered list
+        const randomFeature = riskFeatures[Math.floor(Math.random() * riskFeatures.length)];
+
+        // Fly the map to the selected feature
+        map.flyTo({
+            center: randomFeature.geometry.coordinates[0][0], // Adjust the coordinates if needed
+            zoom: 14, // Adjust the zoom level as desired
+            essential: true // ensures the flyTo animation is smooth
+        });
+    } else {
+        console.log("No census tracts found in the 'risk' category.");
+    }
+}
+*/
